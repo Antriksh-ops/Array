71a0a9744da36874c55bcc9c15901246ae07229c
Vishu <antriksh058@gmail.com>
2025-08-31 13:51:55 +0530

    leet Ques

--- VecSTATDYN.cpp
+++ VecSTATDYN.cpp
@@ -5,6 +5,14 @@ using namespace std;
 int main(){
 
     int arr[5]; // this is static 
-    vector<int> vec1; // vectors are dynamic allocated
+    vector<int> vec; // vectors are dynamic allocated
 
+    vec.push_back(0);
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(3);
+    vec.push_back(4);
+
+    cout<<vec.size()<<endl;
+    cout<<vec.capacity();
 }
