98faf0b00df4f175ce2c829a1fee4dda3e44b642
Vishu <antriksh058@gmail.com>
2025-09-01 12:58:50 +0530

    Majority element using brute

--- MajorityElm.cpp
+++ MajorityElm.cpp
@@ -1,3 +1,23 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
+// brute force 
+int main(){
+    vector<int> v = {1,2,2,6,6,6};
+    int n = v.size();
+    for(int val : v){
+        int freq = 0;
+        for(int el : v){
+            if(el == val){
+                freq++;
+            }
+        }
+        if(freq >= n/2){
+            cout<< val;
+            break;
+        }
+    }
+
+
+}
